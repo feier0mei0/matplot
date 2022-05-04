e7677bfe70f464acd309a800060bb03944a2278c
lixifeng <lixifah@163.com>
2022-05-04 20:30:37 +0800

    使用封装后的requests模块，下载数据

--- csvdata/btc_close_2017.py
+++ csvdata/btc_close_2017.py
@@ -1,20 +1,12 @@
 # -- coding: utf-8 --
 
-from __future__ import (absolute_import, division, print_function, unicode_literals)
-
-try:
-    from urllib2 import urlopen
-except ImportError:
-    from urllib.request import urlopen
-import json
+import requests
 
 # 无法访问该url
 json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
-response = urlopen(json_url)
+req = requests.get(json_url)
 
-req = response.read()
-
-with open('btc_close_2017_urllib.json', 'wb') as f:
-    f.write(req)
-file_urllib = json.loads(req)
-print(file_urllib)
+with open('btc_close_2017_request.json', 'w') as f:
+    f.write(req.text)
+file_requests = req.json(req)
+print(file_requests)
