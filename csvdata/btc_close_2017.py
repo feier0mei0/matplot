df0bbe04e5995807eb052211e7cf773335bce62b
lixifeng <lixifah@163.com>
2022-05-04 21:08:01 +0800

    收盘价折线图、对数变换折线图

--- csvdata/btc_close_2017.py
+++ csvdata/btc_close_2017.py
@@ -9,6 +9,16 @@ filename = 'btc_close_2017.json'
 with open(filename) as f:
     btc_data = json.load(f)
 
+for btc_dict in btc_data:
+    date = btc_dict['date']
+    month = btc_dict['month']
+    week = btc_dict['week']
+    weekday = btc_dict['weekday']
+    close = btc_dict['close']
+    print("{} is month {} week {},{}, the close price is {}RMB".format(date, month, week, weekday, close))
+
+
+# 收盘价折线图
 dates = []
 months = []
 weeks = []
@@ -22,6 +32,16 @@ for btc_dict in btc_data:
     weekdays.append(btc_dict['weekday'])
     close.append(int(float(btc_dict['close'])))
 
+line_chart = pygal.Line(x_label_rotation=20, show_minor_x_labels=False)
+line_chart.title = '收盘价(￥)'
+line_chart.x_labels = dates
+
+N = 20  # x轴每隔20天显示一次
+line_chart.x_labels_major = dates[::N]
+line_chart.add('收盘价', close)
+line_chart.render_to_file('收盘价折线图(￥).svg')
+
+# 收盘价对数变换折线图
 line_chart = pygal.Line(x_label_rotation=20, show_minor_x_labels=False)
 line_chart.title = '收盘价(￥)'
 line_chart.x_labels = dates
