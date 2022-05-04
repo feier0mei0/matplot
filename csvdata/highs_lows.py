680433e6635d124176fe2a08a120d332f867be05
lixifeng <lixifah@163.com>
2022-05-04 10:53:13 +0800

    2014一年的天气数据

--- csvdata/highs_lows.py
+++ csvdata/highs_lows.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # 从文件中获取日期和最高气温
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -23,7 +23,7 @@ fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, 'red')
 
 # 设置图线的格式
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=12)
