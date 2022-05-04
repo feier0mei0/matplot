bc76fb1c4c36a2e270cde8a9ee06f3734beb1595
lixifeng <lixifah@163.com>
2022-05-04 20:40:37 +0800

    读取json文件的内容并打印出来

--- csvdata/btc_close_2017.py
+++ csvdata/btc_close_2017.py
@@ -1,12 +1,16 @@
 # -- coding: utf-8 --
 
-import requests
+import json
 
-# 无法访问该url
-json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
-req = requests.get(json_url)
+# 将数据存储到列表
+filename = 'btc_close_2017.json'
+with open(filename) as f:
+    btc_data = json.load(f)
 
-with open('btc_close_2017_request.json', 'w') as f:
-    f.write(req.text)
-file_requests = req.json(req)
-print(file_requests)
+for btc_dict in btc_data:
+    date = btc_dict['date']
+    month = btc_dict['month']
+    week = btc_dict['week']
+    weekday = btc_dict['weekday']
+    close = btc_dict['close']
+    print("{} is month {} week {},{}, the close price is {}RMB".format(date, month, week, weekday, close))
