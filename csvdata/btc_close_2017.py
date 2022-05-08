f5809094cb6a2c2d40be3554598879ead0a0e20e
lixifeng <lixifah@163.com>
2022-05-08 18:21:14 +0800

    收盘价月日均值、周日均值

--- csvdata/btc_close_2017.py
+++ csvdata/btc_close_2017.py
@@ -3,6 +3,7 @@
 import json
 import pygal
 import math
+from itertools import groupby
 
 # 将数据存储到列表
 filename = 'btc_close_2017.json'
@@ -51,3 +52,27 @@ line_chart.x_labels_major = dates[::N]
 close_log = [math.log10(_) for _ in close]
 line_chart.add('log收盘价', close_log)
 line_chart.render_to_file('收盘价对数变换折线图(￥).svg')
+
+
+def draw_line(x_data, y_data, title, y_legend):
+    xy_map = []
+    for x, y in groupby(sorted(zip(x_data, y_data)), key=lambda _: _[0]):
+        y_list = [v for _, v in y]
+        xy_map.append([x, sum(y_list) / len(y_list)])
+    x_unique, y_mean = [*zip(*xy_map)]
+    line_chart = pygal.Line()
+    line_chart.title = title
+    line_chart.x_labels = x_unique
+
+    line_chart.add(y_legend, y_mean)
+    line_chart.render_to_file(title+'.svg')
+    return line_chart
+
+
+idx_month = dates.index('2017-12-01')
+line_chart_month = draw_line(months[:idx_month], close[:idx_month], '收盘价月日均值（￥）', '月日均值')
+line_chart_month
+
+idx_week = dates.index('2017-12-01')
+line_chart_week = draw_line(weeks[1:idx_week], close[1:idx_week], '收盘价周日均值（￥）', '周日均值')
+line_chart_week
