fbb0b677bacd23486e63a12248f85372b56f5cac
lixifeng <lixifah@163.com>
2022-05-22 21:00:51 +0800

    鼠标放在折线图上，展示热门git项目的描述

--- csvdata/python_repos.py
+++ csvdata/python_repos.py
@@ -15,12 +15,15 @@ print(response_dict["total_count"])
 repo_dicts = response_dict['items']
 print(len(repo_dicts))
 
-repo_dict = repo_dicts[0]
-
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description']
+    }
+    plot_dicts.append(plot_dict)
 
 # able to see
 my_style = LS('#333366', base_style=LCS)
@@ -38,5 +41,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
